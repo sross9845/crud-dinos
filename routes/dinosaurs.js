6bb76ef43d78b95e722361768eb913300bcbc8c5
sross9845 <sean.ross.985@gmail.com>
2019-10-08 14:03:42 -0700

    added show route and new route

--- routes/dinosaurs.js
+++ routes/dinosaurs.js
@@ -10,4 +10,15 @@ router.get('/', function(req,res){
     res.render('dinosaurs/index', {dinos: dinoData});
 });
 
+router.get('/new', function(req,res){
+    res.render('dinosaurs/new')
+});
+
+router.get('/:id', function(req,res){
+    var index = parseInt(req.params.id);
+    var dinos = fs.readFileSync('./dinosaurs.json');
+    var dinoData = JSON.parse(dinos);
+    res.render('dinosaurs/show', {dino: dinoData[index]});
+});
+
 module.exports = router;
